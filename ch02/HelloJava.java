a84f2f7d88c53044a89edb11e6cd3e5446672208
Ryuu Gaku <ryuugaku@gmail.com>
2023-02-08 19:08:34 +0900

    label modified

--- ch02/HelloJava.java
+++ ch02/HelloJava.java
@@ -8,7 +8,7 @@ import javax.swing.*;
 public class HelloJava {
     public static void main( String[] args ) {
         JFrame frame = new JFrame( "Hello, Java!" );
-        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER );
+        JLabel label = new JLabel("Hello, SNS soft !", JLabel.CENTER );
         frame.add(label);
         frame.setSize( 300, 300 );
         frame.setVisible( true );
